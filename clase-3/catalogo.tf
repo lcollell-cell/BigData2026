dc0243d0da461b7ada0d036aaa8f080924fb6dee
Guillermo Henrion <guillermo.henrion@gmail.com>
2026-03-18 10:48:13 -0300

    clase 3

---
+++ clase-3/catalogo.tf
@@ -0,0 +1,7 @@
+resource "databricks_volume" "mlops_volume" {
+  name             = "mi_volume"
+  catalog_name     = "mlops_dev"
+  schema_name      = "default"   # o el schema que uses
+  volume_type      = "MANAGED"  # o EXTERNAL
+  comment          = "Volume para MLOps"
+}
